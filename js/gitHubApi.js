24d9f2beccabf9c199c5f368c296fb947197643a
Rafael Santos <rafaelcachorroquentefoda@gmail.com>
2025-05-22 01:43:19 -0300

    refactory structure of html and update body for grid-layout

--- js/gitHubApi.js
+++ js/gitHubApi.js
@@ -1,37 +1,55 @@
-const projectsContainer = document.getElementById('projects-container')
+const projectsContainer = document.getElementById("projects-container");
 
+const msnry = new Masonry("#projects-container", {
+  itemSelector: ".grid-item",
+  columnWidth: ".grid-sizer",
+  percentPosition: true,
+});
 
+function getApiGitHub() {
+  fetch("https://api.github.com/users/RafaelDesenvolvedor1/repos").then(
+    async (res) => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
 
-function getApiGitHub(){
-    fetch('https://api.github.com/users/RafaelDesenvolvedor1/repos')
-        .then(async res => {
-            if(!res.ok){
-                throw new Error(res.status)
-            }
+      let data = await res.json();
 
-            let data = await res.json()
+      //console.log(data)
 
-            //console.log(data)
+      data
+        .filter((i) => i.private === false)
+        .map((item) => {
+          let project = document.createElement("div");
 
-            data.filter(i => i.private === false).map(item => {
-                let project = document.createElement('div')
-        
-
-                project.innerHTML =` <div class="card grid-item" style="width: 18rem;" data-aos="fade-up" >
+          project.innerHTML = ` <div class="card grid-item" style="width: 18rem;" data-aos="fade-up" >
             <div class="card-body">
                 <h5 class="card-title">${item.name}</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">${item.topics.map(t => innerHTML=`<span class='badge text-bg-primary'>${t}</span>`).join(' ')}</h6>
+                <h6 class="card-subtitle mb-2 text-body-secondary">${item.topics
+                  .map(
+                    (t) =>
+                      (innerHTML = `<span class='badge text-bg-primary'>${t}</span>`)
+                  )
+                  .join(" ")}</h6>
 
                 <p class="card-text">
             ${item.description}</p>
-                <a href="${item.homepage }" target="blank" class="card-link"><i class="fa-solid fa-link"></i></a>
-                <a href="${item.html_url}" target="blank" class="card-link"><i class="fa-brands fa-github"></i></a>
+                <a href="${
+                  item.homepage
+                }" target="blank" class="card-link"><i class="fa-solid fa-link"></i></a>
+                <a href="${
+                  item.html_url
+                }" target="blank" class="card-link"><i class="fa-brands fa-github"></i></a>
             </div>
-        </div>`
+        </div>`;
 
-                projectsContainer.appendChild(project)
-            })
-        })
+          projectsContainer.appendChild(project);
+          msnry.appended(project)
+        });
+
+        msnry.layout();
+    }
+  );
 }
 
-getApiGitHub()
+getApiGitHub();
--- js/toggle.js
+++ js/toggle.js
@@ -1,35 +1,26 @@
-const toggle = $("#toggle")
+const toggle = $("#toggle");
 
-const body = $('body')
-const app = $(".app")
-const box2 = $('.box2')
-const nome = $('.name')
-const cargo = $('.cargo')
-const btnPrimary = $('.btn.primary')
-const btnSecond = $('.btn.second')
-const meusProjetos = $('.my-projects h3')
-const socialMedia = $('.social-media')
-const footer = $('footer')
-
+const body = $("body");
+const main = $("main");
+const nome = $(".name");
+const cargo = $(".cargo");
+const btnPrimary = $(".btn.primary");
+const btnSecond = $(".btn.second");
+const meusProjetos = $("main h3");
+const socialMedia = $(".social-media");
+const footer = $("footer");
 
 toggle.click(() => {
+  // Adiciona a classe dark-mode
 
-
-    // Adiciona a classe dark-mode 
-
-    body.toggleClass('dark-mode')
-    app.toggleClass('dark-mode')
-    box2.toggleClass('dark-mode')
-    nome.toggleClass('dark-mode')
-    cargo.toggleClass('dark-mode')
-    btnPrimary.toggleClass('dark-mode')
-    btnSecond.toggleClass('dark-mode')
-    meusProjetos.toggleClass('dark-mode')
-    socialMedia.toggleClass('dark-mode')
-    toggle.toggleClass('dark-mode')
-    footer.toggleClass('dark-mode')
-
-
-
-
-})
+  body.toggleClass("dark-mode");
+  main.toggleClass("dark-mode");
+  nome.toggleClass("dark-mode");
+  cargo.toggleClass("dark-mode");
+  btnPrimary.toggleClass("dark-mode");
+  btnSecond.toggleClass("dark-mode");
+  meusProjetos.toggleClass("dark-mode");
+  socialMedia.toggleClass("dark-mode");
+  toggle.toggleClass("dark-mode");
+  footer.toggleClass("dark-mode");
+});
